9b6c5e553152a40cf8035107c707119fc1ce5dd7
drv0228 <110354201+drv0228@users.noreply.github.com>
2024-01-15 23:16:08 -0500

    Create StartGame page for Memory game (mobile)
    - Establish the initial structure of the StartGame page
    - Design and style the mobile viewport layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StartGamePage from "./pages/StartGamePage/StartGamePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartGamePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/StartGamePage/StartGamePage.js
@@ -0,0 +1,33 @@
+import "./StartGamePage.scss";
+
+function StartGamePage() {
+    return (
+      <main>
+        <section className="start-game__page">
+        <h1 className="page-title">memory</h1>
+        <div className="page-content">
+            <p className="page-labels">Select Theme</p>
+            <div>
+                <button className="selected">Numbers</button>
+                <button className="">Icons</button>
+            </div>
+            <p className="page-labels">Numbers of Players</p>
+            <div>
+                <button className="button-players selected">1</button>
+                <button className="button-players">2</button>
+                <button className="button-players">3</button>
+                <button className="button-players">4</button>
+            </div>
+            <p className="page-labels">Grid Size</p>
+            <div>
+                <button className="selected">4x4</button>
+                <button>6x6</button>
+            </div>
+            <button className="button-yellow">Start Game</button>
+            </div>
+        </section>
+        </main>
+    );
+  }
+  
+  export default StartGamePage;
