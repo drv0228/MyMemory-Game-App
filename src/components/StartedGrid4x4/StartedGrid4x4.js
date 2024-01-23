af9a2428ac71459eb7cb0363a8d3095c3a5b7fd3
drv0228 <110354201+drv0228@users.noreply.github.com>
2024-01-22 22:12:17 -0500

    WIP: Create Just Started Grid 4x4 component for Memory app (mobile)
    - Establish the initial structure of the Mobile Game - Numbers - Solo - 4x4
    - Implement basic layout and components for solo gameplay 4x4 numbers game
    - Work in progress: Additional enhancements and completion pending

---
+++ src/components/StartedGrid4x4/StartedGrid4x4.js
@@ -0,0 +1,28 @@
+import "./StartedGrid4x4.scss";
+
+function StartedGrid4x4() {
+    return (
+      <>
+        <div className="container">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
+      </>
+    );
+}
+
+    export default StartedGrid4x4;
--- src/pages/SoloPage/SoloPage.js
+++ src/pages/SoloPage/SoloPage.js
@@ -1,11 +1,12 @@
 import "./SoloPage.scss";
 import NavBar from "../../components/NavBar/NavBar";
+import StartedGrid4x4 from "../../components/StartedGrid4x4/StartedGrid4x4";
 
 function SoloPage() {
     return (
       <>
       <NavBar />
-      
+      <StartedGrid4x4 />
       <section className="solo-page__counter">
        <div className="counter-container">
         <p className="counter-labels">Time</p>
